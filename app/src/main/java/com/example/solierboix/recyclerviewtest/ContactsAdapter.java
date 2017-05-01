b94c1362fc7b01ad5df7761341205f8997eee733
Solierboix <solierboix1991@gmail.com>
2017-05-02 00:56:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/solierboix/recyclerviewtest/ContactsAdapter.java
@@ -0,0 +1,104 @@
+package com.example.solierboix.recyclerviewtest;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.solierboix.recyclerviewtest.Contact;
+import com.example.solierboix.recyclerviewtest.R;
+
+import java.util.List;
+
+// Create the basic adapter extending from RecyclerView.Adapter
+// Note that we specify the custom ViewHolder which gives us access to our views
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView nameTextView;
+        public Button messageButton;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            messageButton = (Button) itemView.findViewById(R.id.message_button);
+        }
+
+
+    }
+
+    ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+             viewHolder.getAdapterPosition();
+        }
+    };
+
+
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the Custom layout
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ContactsAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model base on position
+        Contact contact = mContacts.get(position);
+
+        // Set item views basee on your views and data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(contact.getName());
+        Button button = viewHolder.messageButton;
+        button.setText("Message");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+
+    private List<Contact> mContacts;
+    // Store the context for easy access
+    private Context mContext;
+
+    // Pass in the contact array into the constructor
+    public ContactsAdapter(Context context, List<Contact> contacts) {
+        mContacts = contacts;
+        mContext = context;
+    }
+
+    // Easy access to the context object in the recyclerview
+    private Context getContext() {
+        return mContext;
+    }
+}
---
+++ app/src/main/java/com/example/solierboix/recyclerviewtest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.solierboix.recyclerviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts;
+    Button addNewContact;
+    EditText enterNameForNewContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(1);
+        // Create adapter passing in the sample user data
+        final ContactsAdapter adapter = new ContactsAdapter(this, contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rvContacts.addItemDecoration(itemDecoration);
+        // That's all
+
+        enterNameForNewContact = (EditText) findViewById(R.id.EditTextName);
+        addNewContact = (Button) findViewById(R.id.addOneContact);
+
+        addNewContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String editTextName = enterNameForNewContact.getText().toString();
+                if (editTextName.matches("")){
+                    Toast.makeText(MainActivity.this, "Enter name first", Toast.LENGTH_SHORT).show();
+                }else {
+                    contacts.add(0, new Contact(editTextName, true));
+                    adapter.notifyItemInserted(0);
+                    enterNameForNewContact.getText().clear();
+                }
+
+            }
+        });
+    }
+}
